0cacfdaf3656147845a21dc8322277a0fe390157
Markus Kohonen <6650964+maiger@users.noreply.github.com>
2025-10-14 18:11:49 +0300

    Add stats to Details page

--- src/pages/Details.tsx
+++ src/pages/Details.tsx
@@ -20,6 +20,10 @@ type RawFullPokeDetails = {
       name: string;
     };
   }[];
+  stats: {
+    base_stat: number;
+    stat: { name: string };
+  }[];
 };
 
 type FullPokeDetails = {
@@ -29,6 +33,10 @@ type FullPokeDetails = {
   height: number;
   art: string;
   types: string[];
+  stats: {
+    name: string;
+    value: number;
+  }[];
 };
 
 function Details() {
@@ -52,6 +60,12 @@ function Details() {
           height: data.height,
           art: data.sprites.other["official-artwork"].front_default,
           types: data.types.map((type) => type.type.name),
+          stats: data.stats.map((stat) => {
+            return {
+              name: stat.stat.name,
+              value: stat.base_stat,
+            };
+          }),
         };
         setFetchedPokemon(pokeDetails);
       } catch (error) {
@@ -75,10 +89,40 @@ function Details() {
   }
 
   if (fetchedPokemon) {
-    const { name } = fetchedPokemon;
+    const { name, art, weight, height, types, stats } = fetchedPokemon;
+
+    // Stats
+    const hp = stats.find((stat) => stat.name === "hp");
+    const attack = stats.find((stat) => stat.name === "attack");
+    const defence = stats.find((stat) => stat.name === "defense");
+    const s_attack = stats.find((stat) => stat.name === "special-defense");
+    const s_defence = stats.find((stat) => stat.name === "special-attack");
+    const speed = stats.find((stat) => stat.name === "speed");
+
     content = (
-      <div>
-        <h2>{name.charAt(0).toUpperCase() + name.slice(1)}</h2>
+      <div className="flex flex-col items-center">
+        <h2>
+          <span className="pr-1 text-2xl italic text-neutral-700">
+            {"#" + fetchedPokemon.id.toString().padStart(3, "0")}
+          </span>
+          <span className="text-4xl font-bold">
+            {name.charAt(0).toUpperCase() + name.slice(1)}
+          </span>
+        </h2>
+        <img className="w-40" src={art} alt={name} />
+        <div>{weight / 10}kg</div>
+        <div>{height / 10}m</div>
+        <div>
+          {types.map((type) => (
+            <p key={type}>{type}</p>
+          ))}
+        </div>
+        <div>HP: {hp?.value}</div>
+        <div>Attack: {attack?.value}</div>
+        <div>Defence: {defence?.value}</div>
+        <div>Special Attack: {s_attack?.value}</div>
+        <div>Special Defence: {s_defence?.value}</div>
+        <div>Speed: {speed?.value}</div>
       </div>
     );
   }
