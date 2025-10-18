6cf41f3c4c15e22dda1455c336f5944b72d8aa6b
Markus Kohonen <6650964+maiger@users.noreply.github.com>
2025-10-18 19:03:24 +0300

    Refactor EvolutionChainList to use Tanstack Query

--- src/components/EvolutionChainList.tsx
+++ src/components/EvolutionChainList.tsx
@@ -1,6 +1,7 @@
-import { useEffect, useState, type ReactNode } from "react";
+import { useEffect, useState } from "react";
 import ErrorMessage from "./ErrorMessage";
 import { get } from "../util/http";
+import { useQuery } from "@tanstack/react-query";
 
 type EvolutionChainListProps = {
   id: number;
@@ -17,96 +18,80 @@ type EvolutionChainType = {
   chain: EvolutionSpeciesType;
 };
 
+type EvoUrlType = { evolution_chain: { url: string } };
+
+function getEvolutionNames(evoChainData: EvolutionSpeciesType) {
+  let names: string[] = [];
+
+  // Find the end of the chain
+  if (
+    evoChainData.evolves_to === undefined ||
+    evoChainData.evolves_to.length === 0
+  ) {
+    // Return the last name
+    return [evoChainData.species.name];
+  } else {
+    // Not the last element, go deeper
+    const name = getEvolutionNames(evoChainData.evolves_to[0]);
+    names = names.concat(name);
+    // Add the current level evolution name
+    names.push(evoChainData.species.name);
+    // Return all collected names thus far
+    return names;
+  }
+}
+
 function EvolutionChainList({ id }: EvolutionChainListProps) {
   const [fetchedEvoChain, setFetchedEvoChain] = useState<string[]>();
-  const [isFetching, setIsFetching] = useState(false);
-  const [error, setError] = useState<string>();
+
+  // Get evolution chain url
+  const {
+    data: url,
+    isLoading: loadingUrl,
+    error: errorUrl,
+  } = useQuery<EvoUrlType>({
+    queryKey: ["evo-chain-url", id],
+    queryFn: () =>
+      get<EvoUrlType>("https://pokeapi.co/api/v2/pokemon-species/" + id),
+  });
+
+  const evoUrl = url?.evolution_chain.url;
+
+  // Get evolution names
+  const {
+    data: evoNames,
+    isLoading: loadingEvo,
+    error: errorEvo,
+  } = useQuery<EvolutionChainType>({
+    queryKey: ["evo-chain-names", evoUrl],
+    // Enabled makes sure this wont be executed before evoUrl exists, hence "!"
+    queryFn: () => get<EvolutionChainType>(evoUrl!),
+    enabled: !!evoUrl,
+  });
 
   useEffect(() => {
-    function getEvolutionNames(evoChainData: EvolutionSpeciesType) {
-      let names: string[] = [];
-
-      // Find the end of the chain
-      if (
-        evoChainData.evolves_to === undefined ||
-        evoChainData.evolves_to.length === 0
-      ) {
-        // Return the last name
-        return [evoChainData.species.name];
-      } else {
-        // Not the last element, go deeper
-        const name = getEvolutionNames(evoChainData.evolves_to[0]);
-        names = names.concat(name);
-        // Add the current level evolution name
-        names.push(evoChainData.species.name);
-        // Return all collected names thus far
-        return names;
-      }
+    if (evoNames) {
+      const evolutionNames = getEvolutionNames(evoNames.chain);
+      setFetchedEvoChain(evolutionNames.reverse());
     }
+  }, [evoNames]);
 
-    // Fetch evolution chain url
-    async function fetchEvoChainUrl(id: number) {
-      try {
-        const data = (await get(
-          "https://pokeapi.co/api/v2/pokemon-species/" + id
-        )) as { evolution_chain: { url: string } };
-        const evoChainUrl: string = data.evolution_chain.url;
-        return evoChainUrl;
-      } catch (error) {
-        if (error instanceof Error) {
-          setError(error.message);
-          console.log(error);
-        }
-      }
-    }
-
-    // Get the evolution names from the url
-    async function fetchEvoChain(ec_url: string) {
-      try {
-        const data = (await get(ec_url)) as EvolutionChainType;
-        const evoData = getEvolutionNames(data.chain);
-        return evoData;
-      } catch (error) {
-        if (error instanceof Error) {
-          setError(error.message);
-          console.log(error);
-        }
-      }
-    }
-
-    async function fetchAll() {
-      setIsFetching(true);
-
-      const ec_url = await fetchEvoChainUrl(id);
-      console.log(ec_url);
-      if (ec_url) {
-        const evoChain = await fetchEvoChain(ec_url);
-        setFetchedEvoChain(evoChain?.reverse());
-      }
-      setIsFetching(false);
-    }
-
-    fetchAll();
-  }, [id]);
-
-  let content: ReactNode;
-  if (error) content = <ErrorMessage text={error} />;
-
-  if (fetchedEvoChain) {
-    content = (
-      <ul className="flex flex-wrap gap-2">
-        {fetchedEvoChain.map((name) => (
-          <li key={name}>{name.charAt(0).toUpperCase() + name.slice(1)}</li>
-        ))}
-      </ul>
-    );
-  }
-  if (isFetching) content = <p>Loading...</p>;
+  if (loadingUrl || loadingEvo) return <div>Loading...</div>;
+  if (errorUrl instanceof Error)
+    return <ErrorMessage text={errorUrl.message} />;
+  if (errorEvo instanceof Error)
+    return <ErrorMessage text={errorEvo.message} />;
 
   return (
     <div>
       <h3>Evolution Chain</h3>
-      <div>{content}</div>
+      <div>
+        <ul className="flex flex-wrap gap-2">
+          {fetchedEvoChain?.map((name) => (
+            <li key={name}>{name.charAt(0).toUpperCase() + name.slice(1)}</li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
