2589db773f13dbe63e99d1efc0bb656295ed31e3
Markus Kohonen <6650964+maiger@users.noreply.github.com>
2025-10-31 13:16:56 +0200

    Increase TSQ staleTime and gcTime to avoid refetch

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,14 @@ import App from "./App.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 3, // 3 minutes
+      gcTime: 1000 * 60 * 10, // 10 minutes
+    },
+  },
+});
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
